c320e607a2b66971068df55077461239481b3608
Neels-v-Wyk <git@neels.tech>
2024-02-22 22:43:30 +0100

    use docker's compose and not docker-compose

--- IB0302/scripts/create_all_databases.sh
+++ IB0302/scripts/create_all_databases.sh
@@ -9,7 +9,7 @@ for f in `ls /opt/*Create.sql`; do
 	DB=`basename $f | sed 's/Create.sql//'`;
 	[[ "${DB}" == "Security2" ]] && continue;
 	[[ -f /opt/${DB}Insert.sql ]] || continue;
-	echo -n "Creating Databse ${DB}..."
+	echo -n "Creating Database ${DB}..."
 	echo "CREATE DATABASE '/firebird/data/${DB}.fdb' user 'sysdba' password 'ou';" | /usr/local/firebird/bin/isql -q -b
 	/usr/local/firebird/bin/isql -user sysdba -password ou -i /opt/${DB}Create.sql localhost:/firebird/data/${DB}.fdb
 	/usr/local/firebird/bin/isql -user sysdba -password ou -i /opt/${DB}Insert.sql localhost:/firebird/data/${DB}.fdb
